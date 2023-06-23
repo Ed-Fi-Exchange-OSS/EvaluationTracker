0e3f4ac4126e1efd03ed8c8511141e2dc0d4e9b5
StevenM0 <stevenyulongm@gmail.com>
2023-06-23 15:38:17 -0500

    Initial login screen

---
+++ src/reactapp/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import LoginForm from './pages/Login';
+// 1. import `ChakraProvider` component
+import { ChakraProvider } from '@chakra-ui/react'
+import SignupForm from './pages/Signup';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+
+function App() {
+    // 2. Wrap ChakraProvider at the root of my app
+    return (
+        <ChakraProvider>
+            <Router>
+                <Routes>
+                    <Route path="/" element={<SignupForm />} />
+                    <Route path="/login" element={<LoginForm />} />
+                </Routes>
+            </Router>
+        </ChakraProvider>
+    );
+}
+
+export default App;
