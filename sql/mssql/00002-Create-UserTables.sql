e7e0955f96af73b44ea1bacb279cc0554696b263
Stephen Fuqua <stephenfuqua@users.noreply.github.com>
2023-07-12 18:28:35 +0000

    [EPPETA-6] Identity tables for OpenIDDict-based identity (#4)
    * Identity tables for OpenIDDict-based identity
    
    * Add first/last name

---
+++ sql/mssql/00002-Create-UserTables.sql
@@ -0,0 +1,282 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ * Licensed to the Ed-Fi Alliance under one or more agreements.
+ * The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+ * See the LICENSE and NOTICES files in the project root for more information.
+ */
+
+SET ANSI_NULLS ON
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [eppeta].[OpenIddictApplications](
+	[Id] [nvarchar](450) NOT NULL,
+	[ClientId] [nvarchar](100) NULL,
+	[ClientSecret] [nvarchar](max) NULL,
+	[ConcurrencyToken] [nvarchar](50) NULL,
+	[ConsentType] [nvarchar](50) NULL,
+	[DisplayName] [nvarchar](max) NULL,
+	[DisplayNames] [nvarchar](max) NULL,
+	[Permissions] [nvarchar](max) NULL,
+	[PostLogoutRedirectUris] [nvarchar](max) NULL,
+	[Properties] [nvarchar](max) NULL,
+	[RedirectUris] [nvarchar](max) NULL,
+	[Requirements] [nvarchar](max) NULL,
+	[Type] [nvarchar](50) NULL,
+ CONSTRAINT [PK_OpenIddictApplications] PRIMARY KEY CLUSTERED
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+
+
+CREATE TABLE [eppeta].[OpenIddictAuthorizations](
+	[Id] [nvarchar](450) NOT NULL,
+	[ApplicationId] [nvarchar](450) NULL,
+	[ConcurrencyToken] [nvarchar](50) NULL,
+	[CreationDate] [datetime2](7) NULL,
+	[Properties] [nvarchar](max) NULL,
+	[Scopes] [nvarchar](max) NULL,
+	[Status] [nvarchar](50) NULL,
+	[Subject] [nvarchar](400) NULL,
+	[Type] [nvarchar](50) NULL,
+ CONSTRAINT [PK_OpenIddictAuthorizations] PRIMARY KEY CLUSTERED
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+
+ALTER TABLE [eppeta].[OpenIddictAuthorizations]  WITH CHECK ADD  CONSTRAINT [FK_OpenIddictAuthorizations_OpenIddictApplications_ApplicationId] FOREIGN KEY([ApplicationId])
+REFERENCES [eppeta].[OpenIddictApplications] ([Id])
+GO
+
+ALTER TABLE [eppeta].[OpenIddictAuthorizations] CHECK CONSTRAINT [FK_OpenIddictAuthorizations_OpenIddictApplications_ApplicationId]
+GO
+
+
+
+CREATE TABLE [eppeta].[OpenIddictScopes](
+	[Id] [nvarchar](450) NOT NULL,
+	[ConcurrencyToken] [nvarchar](50) NULL,
+	[Description] [nvarchar](max) NULL,
+	[Descriptions] [nvarchar](max) NULL,
+	[DisplayName] [nvarchar](max) NULL,
+	[DisplayNames] [nvarchar](max) NULL,
+	[Name] [nvarchar](200) NULL,
+	[Properties] [nvarchar](max) NULL,
+	[Resources] [nvarchar](max) NULL,
+ CONSTRAINT [PK_OpenIddictScopes] PRIMARY KEY CLUSTERED
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+
+
+
+CREATE TABLE [eppeta].[OpenIddictTokens](
+	[Id] [nvarchar](450) NOT NULL,
+	[ApplicationId] [nvarchar](450) NULL,
+	[AuthorizationId] [nvarchar](450) NULL,
+	[ConcurrencyToken] [nvarchar](50) NULL,
+	[CreationDate] [datetime2](7) NULL,
+	[ExpirationDate] [datetime2](7) NULL,
+	[Payload] [nvarchar](max) NULL,
+	[Properties] [nvarchar](max) NULL,
+	[RedemptionDate] [datetime2](7) NULL,
+	[ReferenceId] [nvarchar](100) NULL,
+	[Status] [nvarchar](50) NULL,
+	[Subject] [nvarchar](400) NULL,
+	[Type] [nvarchar](50) NULL,
+ CONSTRAINT [PK_OpenIddictTokens] PRIMARY KEY CLUSTERED
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+
+ALTER TABLE [eppeta].[OpenIddictTokens]  WITH CHECK ADD  CONSTRAINT [FK_OpenIddictTokens_OpenIddictApplications_ApplicationId] FOREIGN KEY([ApplicationId])
+REFERENCES [eppeta].[OpenIddictApplications] ([Id])
+GO
+
+ALTER TABLE [eppeta].[OpenIddictTokens] CHECK CONSTRAINT [FK_OpenIddictTokens_OpenIddictApplications_ApplicationId]
+GO
+
+ALTER TABLE [eppeta].[OpenIddictTokens]  WITH CHECK ADD  CONSTRAINT [FK_OpenIddictTokens_OpenIddictAuthorizations_AuthorizationId] FOREIGN KEY([AuthorizationId])
+REFERENCES [eppeta].[OpenIddictAuthorizations] ([Id])
+GO
+
+ALTER TABLE [eppeta].[OpenIddictTokens] CHECK CONSTRAINT [FK_OpenIddictTokens_OpenIddictAuthorizations_AuthorizationId]
+GO
+
+
+
+CREATE TABLE [eppeta].[Roles](
+	[Id] [nvarchar](225) NOT NULL,
+	[Name] [nvarchar](256) NULL,
+	[NormalizedName] [nvarchar](256) NULL,
+	[ConcurrencyStamp] [nvarchar](max) NULL,
+ CONSTRAINT [PK_Roles] PRIMARY KEY CLUSTERED
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+
+
+
+INSERT INTO [eppeta].[Roles] ([Id], [Name], [NormalizedName])
+VALUES ('1', 'Administrator', 'ADMINISTRATOR'), ('2', 'Supervisor', 'SUPERVISOR'), ('3', 'Mentor Teacher', 'MENTOR TEACHER');
+GO
+
+
+
+CREATE TABLE [eppeta].[RoleClaims](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[RoleId] [nvarchar](225) NOT NULL,
+	[ClaimType] [nvarchar](max) NULL,
+	[ClaimValue] [nvarchar](max) NULL,
+ CONSTRAINT [PK_RoleClaims] PRIMARY KEY CLUSTERED
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+
+ALTER TABLE [eppeta].[RoleClaims]  WITH CHECK ADD  CONSTRAINT [FK_RoleClaims_Roles_RoleId] FOREIGN KEY([RoleId])
+REFERENCES [eppeta].[Roles] ([Id])
+ON DELETE CASCADE
+GO
+
+ALTER TABLE [eppeta].[RoleClaims] CHECK CONSTRAINT [FK_RoleClaims_Roles_RoleId]
+GO
+
+-- TODO: create some initial role claims.
+
+
+
+CREATE TABLE [eppeta].[Users](
+	[Id] [nvarchar](225) NOT NULL,
+	[RequirePasswordChange] [bit] NOT NULL,
+	[UserName] [nvarchar](256) NULL,
+	[NormalizedUserName] [nvarchar](256) NULL,
+	[Email] [nvarchar](256) NULL,
+	[NormalizedEmail] [nvarchar](256) NULL,
+	[EmailConfirmed] [bit] NOT NULL,
+	[PasswordHash] [nvarchar](max) NULL,
+	[SecurityStamp] [nvarchar](max) NULL,
+	[ConcurrencyStamp] [nvarchar](max) NULL,
+	[PhoneNumber] [nvarchar](max) NULL,
+	[PhoneNumberConfirmed] [bit] NOT NULL,
+	[TwoFactorEnabled] [bit] NOT NULL,
+	[LockoutEnd] [datetimeoffset](7) NULL,
+	[LockoutEnabled] [bit] NOT NULL,
+	[AccessFailedCount] [int] NOT NULL,
+    [FirstName] [nvarchar](100) NULL,
+    [LastName] [nvarchar](100) NULL,
+ CONSTRAINT [PK_Users] PRIMARY KEY CLUSTERED
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+
+
+
+CREATE TABLE [eppeta].[UserRoles](
+	[UserId] [nvarchar](225) NOT NULL,
+	[RoleId] [nvarchar](225) NOT NULL,
+ CONSTRAINT [PK_UserRoles] PRIMARY KEY CLUSTERED
+(
+	[UserId] ASC,
+	[RoleId] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [eppeta].[UserRoles]  WITH CHECK ADD  CONSTRAINT [FK_UserRoles_Roles_RoleId] FOREIGN KEY([RoleId])
+REFERENCES [eppeta].[Roles] ([Id])
+ON DELETE CASCADE
+GO
+
+ALTER TABLE [eppeta].[UserRoles] CHECK CONSTRAINT [FK_UserRoles_Roles_RoleId]
+GO
+
+ALTER TABLE [eppeta].[UserRoles]  WITH CHECK ADD  CONSTRAINT [FK_UserRoles_Users_UserId] FOREIGN KEY([UserId])
+REFERENCES [eppeta].[Users] ([Id])
+ON DELETE CASCADE
+GO
+
+ALTER TABLE [eppeta].[UserRoles] CHECK CONSTRAINT [FK_UserRoles_Users_UserId]
+GO
+
+
+
+CREATE TABLE [eppeta].[UserLogins](
+	[LoginProvider] [nvarchar](450) NOT NULL,
+	[ProviderKey] [nvarchar](450) NOT NULL,
+	[ProviderDisplayName] [nvarchar](max) NULL,
+	[UserId] [nvarchar](225) NOT NULL,
+ CONSTRAINT [PK_UserLogins] PRIMARY KEY CLUSTERED
+(
+	[LoginProvider] ASC,
+	[ProviderKey] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+
+ALTER TABLE [eppeta].[UserLogins]  WITH CHECK ADD  CONSTRAINT [FK_UserLogins_Users_UserId] FOREIGN KEY([UserId])
+REFERENCES [eppeta].[Users] ([Id])
+ON DELETE CASCADE
+GO
+
+ALTER TABLE [eppeta].[UserLogins] CHECK CONSTRAINT [FK_UserLogins_Users_UserId]
+GO
+
+
+
+CREATE TABLE [eppeta].[UserClaims](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[UserId] [nvarchar](225) NOT NULL,
+	[ClaimType] [nvarchar](max) NULL,
+	[ClaimValue] [nvarchar](max) NULL,
+ CONSTRAINT [PK_UserClaims] PRIMARY KEY CLUSTERED
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+
+ALTER TABLE [eppeta].[UserClaims]  WITH CHECK ADD  CONSTRAINT [FK_UserClaims_Users_UserId] FOREIGN KEY([UserId])
+REFERENCES [eppeta].[Users] ([Id])
+ON DELETE CASCADE
+GO
+
+ALTER TABLE [eppeta].[UserClaims] CHECK CONSTRAINT [FK_UserClaims_Users_UserId]
+GO
+
+
+
+CREATE TABLE [eppeta].[UserTokens](
+	[UserId] [nvarchar](225) NOT NULL,
+	[LoginProvider] [nvarchar](112) NOT NULL,
+	[Name] [nvarchar](112) NOT NULL,
+	[Value] [nvarchar](max) NULL,
+ CONSTRAINT [PK_UserTokens] PRIMARY KEY CLUSTERED
+(
+	[UserId] ASC,
+	[LoginProvider] ASC,
+	[Name] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+
+ALTER TABLE [eppeta].[UserTokens]  WITH CHECK ADD  CONSTRAINT [FK_UserTokens_Users_UserId] FOREIGN KEY([UserId])
+REFERENCES [eppeta].[Users] ([Id])
+ON DELETE CASCADE
+GO
+
+ALTER TABLE [eppeta].[UserTokens] CHECK CONSTRAINT [FK_UserTokens_Users_UserId]
+GO
