4c1ab913c93e98b5173dc19be88bcdf4cfbfb481
Stephen A. Fuqua <stephen@safnet.com>
2023-10-20 14:50:21 -0500

    Update to match our coding style

--- src/webapi/TokenRetriever.cs
+++ src/webapi/TokenRetriever.cs
@@ -7,29 +7,29 @@ namespace EdFi.OdsApi.SdkClient
 {
     public class TokenRetriever
     {
-        private string oauthUrl;
-        private string clientKey;
-        private string clientSecret;
+        private readonly string _oauthUrl;
+        private readonly string _clientKey;
+        private readonly string _clientSecret;
 
         public TokenRetriever(string oauthUrl, string clientKey, string clientSecret)
         {
-            this.oauthUrl = oauthUrl;
-            this.clientKey = clientKey;
-            this.clientSecret = clientSecret;
+            _oauthUrl = oauthUrl;
+            _clientKey = clientKey;
+            _clientSecret = clientSecret;
         }
 
         public string ObtainNewBearerToken()
         {
-            var oauthClient = new ApiClient(oauthUrl);
+            var oauthClient = new ApiClient(_oauthUrl);
             return GetBearerToken(oauthClient);
         }
 
         private string GetBearerToken(ApiClient oauthClient)
         {
-            var configuration = new Configuration() { BasePath = oauthUrl };
+            var configuration = new Configuration() { BasePath = _oauthUrl };
             var bearerTokenRequestOptions = new RequestOptions() { Operation = String.Empty };
-            bearerTokenRequestOptions.FormParameters.Add("Client_id", clientKey);
-            bearerTokenRequestOptions.FormParameters.Add("Client_secret", clientSecret);
+            bearerTokenRequestOptions.FormParameters.Add("Client_id", _clientKey);
+            bearerTokenRequestOptions.FormParameters.Add("Client_secret", _clientSecret);
             bearerTokenRequestOptions.FormParameters.Add("Grant_type", "client_credentials");
 
             var bearerTokenResponse = oauthClient.Post<BearerTokenResponse>("oauth/token", bearerTokenRequestOptions, configuration);
@@ -53,15 +53,15 @@ namespace EdFi.OdsApi.SdkClient
     internal class BearerTokenResponse
     {
         [DataMember(Name = "access_token", EmitDefaultValue = false)]
-        public string AccessToken { get; set; }
+        public string AccessToken { get; set; } = string.Empty;
 
         [DataMember(Name = "expires_in", EmitDefaultValue = false)]
-        public string ExpiresIn { get; set; }
+        public string ExpiresIn { get; set; } = string.Empty;
 
         [DataMember(Name = "token_type", EmitDefaultValue = false)]
-        public string TokenType { get; set; }
+        public string TokenType { get; set; } = string.Empty;
 
         [DataMember(Name = "error", EmitDefaultValue = false)]
-        public string Error { get; set; }
+        public string Error { get; set; } = string.Empty;
     }
 }
