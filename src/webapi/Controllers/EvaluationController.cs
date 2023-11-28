7b03cad0d6fc9b9e80f206024393f8dd1d7d4a28
Marcos Gallo <mgallo@crocusllc.com>
2023-11-28 12:27:46 -0300

    Some error hanlding and improvements on Saving and Sending objects.

--- src/webapi/Controllers/EvaluationController.cs
+++ src/webapi/Controllers/EvaluationController.cs
@@ -11,6 +11,7 @@ using eppeta.webapi.Evaluations.Models;
 using Microsoft.Extensions.Caching.Memory;
 using eppeta.webapi.DTO;
 using System.Linq.Dynamic.Core;
+using System.Linq.Expressions;
 
 namespace webapi.Controllers;
 
@@ -111,40 +112,47 @@ public class EvaluationController : ControllerBase
     [HttpGet("Sync")]
     public async Task<ActionResult> SyncEvaluationComponents()
     {
-        // Check if already synced dependencies in cache
-        if (_memoryCache.Get(dataExpirationKey) == null)
+        try
         {
-            // Refresh Evaluation data from API
-            // Get ODS/API token
-            var authenticatedConfiguration = await _service.GetAuthenticatedConfiguration();
+            // Check if already synced dependencies in cache
+            if (_memoryCache.Get(dataExpirationKey) == null)
+            {
+                // Refresh Evaluation data from API
+                // Get ODS/API token
+                var authenticatedConfiguration = await _service.GetAuthenticatedConfiguration();
 
-            //// Get Evaluation Objectives and update repository
-            var objectivesApi = new EvaluationObjectivesApi(authenticatedConfiguration);
-            objectivesApi.Configuration.DefaultHeaders.Add("Content-Type", "application/json");
-            var tpdmEvaluationObjectives = await objectivesApi.GetEvaluationObjectivesAsync(limit: 100, offset: 0);
-            await _evaluationRepository.UpdateEvaluationObjectives(tpdmEvaluationObjectives.Select(teo => (EvaluationObjective)teo).ToList());
+                //// Get Evaluation Objectives and update repository
+                var objectivesApi = new EvaluationObjectivesApi(authenticatedConfiguration);
+                objectivesApi.Configuration.DefaultHeaders.Add("Content-Type", "application/json");
+                var tpdmEvaluationObjectives = await objectivesApi.GetEvaluationObjectivesAsync(limit: 100, offset: 0);
+                await _evaluationRepository.UpdateEvaluationObjectives(tpdmEvaluationObjectives.Select(teo => (EvaluationObjective)teo).ToList());
 
-            // Get Evaluation Elements which contain the EvaluationObjectiveTitles and update repository
-            var elementsApi = new EvaluationElementsApi(authenticatedConfiguration);
-            elementsApi.Configuration.DefaultHeaders.Add("Content-Type", "application/json");
-            var tpdmEvaluationElements = await elementsApi.GetEvaluationElementsAsync(limit: 100, offset: 0);
-            await _evaluationRepository.UpdateEvaluationElements(tpdmEvaluationElements.Select(tee => (EvaluationElement)tee).ToList());
+                // Get Evaluation Elements which contain the EvaluationObjectiveTitles and update repository
+                var elementsApi = new EvaluationElementsApi(authenticatedConfiguration);
+                elementsApi.Configuration.DefaultHeaders.Add("Content-Type", "application/json");
+                var tpdmEvaluationElements = await elementsApi.GetEvaluationElementsAsync(limit: 100, offset: 0);
+                await _evaluationRepository.UpdateEvaluationElements(tpdmEvaluationElements.Select(tee => (EvaluationElement)tee).ToList());
 
-            var peApi = new PerformanceEvaluationsApi(authenticatedConfiguration);
-            peApi.Configuration.DefaultHeaders.Add("Content-Type", "application/json");
-            var tpdmPerformanceEvaluations = await peApi.GetPerformanceEvaluationsAsync(limit: 100, offset: 0);
-            var performanceEvaluations = tpdmPerformanceEvaluations.Select(pe => (PerformanceEvaluation)pe).ToList();
-            await _evaluationRepository.UpdatePerformanceEvaluations(performanceEvaluations);
-            // set next expiration time
-            var cachedValue = _memoryCache.GetOrCreate(
-                dataExpirationKey,
-                cacheEntry =>
-                {
-                    cacheEntry.AbsoluteExpirationRelativeToNow = dataExpirationInterval;
-                    return DateTime.Now;
-                });
+                var peApi = new PerformanceEvaluationsApi(authenticatedConfiguration);
+                peApi.Configuration.DefaultHeaders.Add("Content-Type", "application/json");
+                var tpdmPerformanceEvaluations = await peApi.GetPerformanceEvaluationsAsync(limit: 100, offset: 0);
+                var performanceEvaluations = tpdmPerformanceEvaluations.Select(pe => (PerformanceEvaluation)pe).ToList();
+                await _evaluationRepository.UpdatePerformanceEvaluations(performanceEvaluations);
+                // set next expiration time
+                var cachedValue = _memoryCache.GetOrCreate(
+                    dataExpirationKey,
+                    cacheEntry =>
+                    {
+                        cacheEntry.AbsoluteExpirationRelativeToNow = dataExpirationInterval;
+                        return DateTime.Now;
+                    });
+                return Ok();
+            }
         }
-        return Ok();
-
+        catch (Exception ex)
+        {
+            return Problem(ex.Message);
+        }
+        return Problem();
     }
 }
