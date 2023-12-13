6795c9093128f0e0f161e7ea0a7d6fc6627717e0
David Jimenez Barrantes <djimenez@growthaccelerationpartners.com>
2023-12-13 16:40:53 -0600

    Changes on save Performance Evaluation - Update

--- src/webapi/DTO/PerformedEvaluationResult.cs
+++ src/webapi/DTO/PerformedEvaluationResult.cs
@@ -17,6 +17,7 @@ namespace eppeta.webapi.DTO
         public string ReviewedPersonSourceSystemDescriptor { get; set; } = string.Empty;
         public string ReviewedCandidateName { get; set; } = string.Empty;
         public int PerformanceEvaluationId { get; set; }
+        public int PerformanceEvaluationRatingId { get; set; }
         public int EvaluationId { get; set; }
         public string PerformanceEvaluationTitle { get; set; } = string.Empty;
         public int? StatusId { get; set; }
