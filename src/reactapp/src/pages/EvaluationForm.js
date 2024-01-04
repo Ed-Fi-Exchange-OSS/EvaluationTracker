498567405bae30c036c4a33a3959062ab88f4a1b
David Jimenez Barrantes <djimenez@growthaccelerationpartners.com>
2024-01-04 10:57:56 -0600

    Front end in progress.

--- src/reactapp/src/pages/EvaluationForm.js
+++ src/reactapp/src/pages/EvaluationForm.js
@@ -252,6 +252,7 @@ export default function EvaluationForm() {
     completedEvaluation.reviewedPersonSourceSystemDescriptor = selectedCandidate.sourceSystemDescriptor;
     completedEvaluation.evaluatorName = currentEvaluator.evaluatorName;
     completedEvaluation.reviewedCandidateName = selectedCandidate.candidateName;
+    completedEvaluation.comments = evaluationDataLoaded.comments;
     completedEvaluation.startDateTime = evaluationDataLoaded.evaluationDate;
     !evaluationDataLoaded.evaluationEndTime ?
       completedEvaluation.endDateTime = new Date() : completedEvaluation.endDateTime = evaluationDataLoaded.evaluationEndTime;
@@ -314,6 +315,18 @@ export default function EvaluationForm() {
       const locatedIndex = objectiveNotes.findIndex((obj) => obj.objectiveId === objective.evaluationObjectiveId);
       return locatedIndex >= 0 ? objectiveNotes[locatedIndex].value.length : "0";
   }
+
+  /**
+   * Event to update global evaluation comments
+   * @param {any} e
+   */
+  const handleEvaluationCommentsUpdates = (e) => {
+    const evaluationDataLoadedCopy = { ...evaluationDataLoaded };
+    evaluationDataLoadedCopy.comments = e.target.value;
+
+    savePageData(evaluationDataLoadedCopy);
+    setEvaluationDataLoaded(evaluationDataLoadedCopy);
+  };
 
   /**
    * Event that updates start date
@@ -797,6 +810,17 @@ export default function EvaluationForm() {
                   </Table>
                 </Box>
               ))}
+
+              <Box mt={4}>
+                <Heading fontSize="lg" fontWeight="bold">Observation Notes</Heading>
+                <Table cellSpacing="10" cellPadding="5">
+                  <Tbody>
+                    <Tr key="globalComments">
+                      <Td rowSpan="4"><Textarea id="globalComments" name="globalComments" onBlur={handleEvaluationCommentsUpdates} rows="3" resize="none" borderColor="gray.300" defaultValue={(performanceEvaluationData ? performanceEvaluationData.comments : "")} /></Td>
+                    </Tr>
+                  </Tbody>
+                </Table>
+              </Box>
             </Box>
           </Box>
           <Box mt="0" textAlign="center">
