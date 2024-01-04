e58f3781226bc25c6c8852d0e5078cab829fa739
José Alberto Leiva Obando <56046999+jleiva-gap@users.noreply.github.com>
2024-01-04 12:21:03 -0600

    EPPETA-21  Redirect to Sign In Page on Token Expiration (#36)
    * EPPETA-21  Redirect to Sign In Page on Token Expiration
    
    Add a component to validate page session.
    Add validation to check exp date for JWT (frontend)
    
    * Update component to validate authenticated routes
    
    Add an element prop to send the autenticated Page

---
+++ src/reactapp/src/components/AuthenticatedRoute.js
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: Apache-2.0
+// Licensed to the Ed-Fi Alliance under one or more agreements.
+// The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+// See the LICENSE and NOTICES files in the project root for more information.
+
+import { useEffect } from "react";
+import { Route, useNavigate } from 'react-router-dom';
+import {
+  useToast,
+} from '@chakra-ui/react';
+import { getToken } from "../components/TokenHelpers";
+
+const AuthenticatedRoute = ({ element: authenticatedComponent, ...rest }) => {
+  let navigate = useNavigate();
+  const toast = useToast();
+
+  useEffect(() => {
+    if (!getToken()) {
+      toast({
+        title: "Session has expired.",
+        description: "Your session has expired.Please log in again to continue.",
+        status: "warning",
+        duration: 5000,
+        isClosable: true,
+      });
+      navigate("/login");
+      sessionStorage.clear();
+    }
+  });
+
+  return <>{authenticatedComponent}</>;
+}
+ export default AuthenticatedRoute;
