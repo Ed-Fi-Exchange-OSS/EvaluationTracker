dd3bb5ce8c1e37142209a957542cb910f157f347
David Jimenez Barrantes <djimenez@growthaccelerationpartners.com>
2024-01-09 09:20:16 -0600

    Renames evaluation status values. (#39)

--- sql/mssql/00010-Ancilliary-Tables.sql
+++ sql/mssql/00010-Ancilliary-Tables.sql
@@ -1,8 +1,8 @@
 CREATE TABLE [eppeta].[Status] (
     [Id] INT IDENTITY(1,1) NOT NULL,
-    [StatusText] [nvarchar](15) NOT NULL
+    [StatusText] [nvarchar](50) NOT NULL
     CONSTRAINT Status_PK PRIMARY KEY CLUSTERED (Id ASC)
     );
-INSERT INTO [eppeta].[Status] ([StatusText]) VALUES ('Not Uploaded'),('Uploaded'),('Failed');
+INSERT INTO [eppeta].[Status] ([StatusText]) VALUES ('Ready for Review'),('Review Approved, transferred to ODS/API'),('Review Approved, transfer to ODS/API failed');
 
 
